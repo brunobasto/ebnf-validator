f0876a830bb125fa3d8e1c0845251ee4b8d55c63
Bruno Basto <bruno.basto@liferay.com>
2014-09-12 15:36:10 -0300

    Configuring embed tomcat to allow servlets defined by annotations

--- src/com/brunobasto/ebnf/server/Launcher.java
+++ src/com/brunobasto/ebnf/server/Launcher.java
@@ -5,22 +5,30 @@ import java.io.File;
 import javax.servlet.ServletException;
 
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.naming.resources.VirtualDirContext;
 public class Launcher {
 
 	public static void main(String[] args) {
 		String webappDirLocation = "docroot/";
 
 		Tomcat tomcat = new Tomcat();
+		tomcat.setPort(8080);
 
-		String webPort = "8080";
-
-		tomcat.setPort(Integer.valueOf(webPort));
-
-		String path = new File("./" + webappDirLocation).getAbsolutePath();
+		String path = new File(webappDirLocation).getAbsolutePath();
 
 		try {
-			tomcat.addWebapp("/", path);
+			StandardContext ctx = (StandardContext)tomcat.addWebapp("/", path);
+
+			File webInfClasses = new File("classes");
+
+			VirtualDirContext resources = new VirtualDirContext();
+
+			resources.setExtraResourcePaths(
+				"/WEB-INF/classes=" + webInfClasses);
+
+			ctx.setResources(resources);
 
 			tomcat.start();
 		}
